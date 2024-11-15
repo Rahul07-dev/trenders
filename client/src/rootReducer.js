662f5d3cd452c4fed23d65ba50d7ea7c1dba3aaa
Rahul07-dev <rahuldev072002@gmail.com>
2024-11-16 00:03:41 +0530

    Removed Clients folder

--- client/src/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from 'redux'
-import userDetailsReducer from './reducers/userDetails'
-import trendingAndSpecialOffersReducer from './reducers/trendingAndSpecialOffers'
-const rootReducer = combineReducers({
-    userDetailsReducer,trendingAndSpecialOffersReducer
-})
-
-export default rootReducer
